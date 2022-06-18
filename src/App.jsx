b1f32a2f02e18ec54dac081f8eddd8d4a1a90a42
phaneesh <phaneesh707@gmail.com>
2022-06-18 16:43:22 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { useState,useEffect } from "react";
+import './App.css'
+import searchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fb48203c";
+
+const App = ()=>{
+    
+    const [movies,setMovies] = useState([])
+    const searchMovie = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()   
+        setMovies(data.Search);
+        console.log(data.Search);
+
+    }
+
+    useEffect(()=>{
+        searchMovie('Avengers');
+    },[]);
+
+    
+    const [srcItem,setItem] = useState("");
+    const handleChange = (e) =>{
+        setItem(e.target.value)
+    }
+
+    const handleClick = () =>{
+        setItem("")
+        searchMovie(srcItem);
+    }
+
+    return(
+        <div className="app">
+            <h1>FILM FLAIR</h1>
+
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value={srcItem}
+                    onChange={handleChange}
+                />
+                <img
+                    src={searchIcon}
+                    alt="search"
+                    onClick={handleClick}
+                />
+           </div>
+
+
+           {
+                movies?.length > 0 ?
+                (
+                <div className="container">
+                {
+                    // <MovieCard mov={movies[0]} />
+                    movies.map((movie)=>(
+                        <MovieCard mov={movie} />
+                    ))
+                }
+                </div>
+            ):(
+                <div className="empty">
+                    <h2>No Movies found</h2>
+                </div>
+            )
+
+           }
+
+            
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MovieCard = ({mov}) =>{
+    return(
+    <div className="movie">
+        <div>
+            <p>{mov.Year}</p>
+        </div>
+        <div>
+            <img src={mov.Poster != 'N/A'?mov.Poster:'https://via.placeholder.com/400'} alt={mov.Title} />
+        </div>
+        <div>
+            <span>{mov.Type}</span>
+            <h3>{mov.Title}</h3>
+        </div>
+    </div>
+)
+}
+
+export default MovieCard;
